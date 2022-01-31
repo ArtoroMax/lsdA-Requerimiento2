c5a915eb207387ed685ce9007a914a33a0ccb0b1
Yahir Garcés <Yahir Garcs@DESKTOP-AUJ7R1V>
2022-01-31 01:36:53 -0500

    cambio dto

--- Requerimiento02/s_gestion_usuarios/sop_rmi/GestionUsuariosInt.java
+++ Requerimiento02/s_gestion_usuarios/sop_rmi/GestionUsuariosInt.java
@@ -5,6 +5,7 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 import s_gestion_usuarios.dto.PersonalDTO;
 import s_gestion_usuarios.dto.UsuarioDTO;
+import s_gestion_usuarios.dto.ValoracionDTO;
 import s_gestion_usuarios.dto.CredencialDTO;
 import cliente.sop_rmi.AdminCllbckInt;
 //Hereda de la clase Remote, lo cual la convierte en interfaz remota
@@ -22,6 +23,8 @@ public interface GestionUsuariosInt extends Remote {
     public PersonalDTO abrirSesion(CredencialDTO objCredencial) throws RemoteException;
 
     public boolean registrarUsuario(UsuarioDTO objUsuario) throws RemoteException;
+
+    public boolean registrarValoracion(ValoracionDTO objValoracion, int id) throws RemoteException;
 
     public UsuarioDTO consultarUsuario(int id) throws RemoteException;
 }
